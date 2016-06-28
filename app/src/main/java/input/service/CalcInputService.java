dd4ea9668f958a6adc7a50fba458b98612952bc5
Yosi Mizrachi <yosi@onoapps.com>
2016-06-28 15:11:07 +0300

    basic arithemic instruction set

--- app/src/main/java/input/service/CalcInputService.java
+++ app/src/main/java/input/service/CalcInputService.java
@@ -18,6 +18,7 @@ import views.CalcKeyboardView;
  */
 public class CalcInputService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
 
+    private static final String TAG = "CalcInputService";
     private InputMethodManager mInputMethodManager;
     private CalcKeyboardView mKeyboardView;
     private CalcKeyboard mKeyboard;
@@ -48,7 +49,6 @@ public class CalcInputService extends InputMethodService implements KeyboardView
     @Override
     public void onStartInput(EditorInfo attribute, boolean restarting) {
         super.onStartInput(attribute, restarting);
-        mTextComposition.setLength(0);
     }
 
     @Override
@@ -66,18 +66,28 @@ public class CalcInputService extends InputMethodService implements KeyboardView
         InputConnection ic = getCurrentInputConnection();
         switch (primaryCode) {
             case Keyboard.KEYCODE_DELETE:
-                ic.deleteSurroundingText(1, 0);
+                reset();
                 break;
             case CalcKeyboard.SETTINGS_KEY:
                 showInputChooser();
                 break;
             case CalcKeyboard.CALCULATE:
+                try {
+                    int result = calculate(mTextComposition.toString());
+                    reset();
+                    mTextComposition.append(result);
+                    ic.commitText(String.valueOf(result), 1);
+                } catch (NumberFormatException exception) {
+                    // "complicated expressions not yet implemented. For now only simple expression will be evaluated.
+                    reset();
+                    ic.commitText(getString(R.string.error), 1);
+                }
                 break;
             default:
                 char code = (char) primaryCode;
                 ic.commitText(String.valueOf(code), 1);
+                mTextComposition.append(code);
         }
-
     }
 
     @Override
@@ -109,5 +119,102 @@ public class CalcInputService extends InputMethodService implements KeyboardView
         if (mInputMethodManager != null) {
             mInputMethodManager.showInputMethodPicker();
         }
+    }
+
+    private int calculate(String input) {
+        int expressionStarts = 0;
+        int expressionEnds = 0;
+        String memberAString = null;
+        String memberBString = null;
+        int result = 0000;
+
+        for (int index = 0; index < input.length(); index++) {
+            char currentChar = input.charAt(index);
+
+            // find the members of the expression
+            if (currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') {
+                expressionStarts = getFirstMemberIndex(input, index - 1);
+                expressionEnds = getSecondMemberIndex(input, index);
+
+                memberAString = input.substring(expressionStarts, index);
+                memberBString = input.substring(index + 1, expressionEnds);
+            }
+
+
+            switch (currentChar) {
+                case '+':
+                    result = add(Integer.valueOf(memberAString), Integer.valueOf(memberBString));
+                    break;
+                case '-':
+                    result = subtract(Integer.valueOf(memberAString), Integer.valueOf(memberBString));
+                    break;
+                case '*':
+                    result = multiply(Integer.valueOf(memberAString), Integer.valueOf(memberBString));
+                    break;
+                case '/':
+                    result = divide(Integer.valueOf(memberAString), Integer.valueOf(memberBString));
+                    break;
+            }
+        }
+        return result;
+    }
+
+
+    private void reset() {
+        mTextComposition.delete(0, mTextComposition.length());
+        getCurrentInputConnection().deleteSurroundingText(1000, 1000);
+    }
+
+    private int getFirstMemberIndex(String input, int index) {
+        if (index == 0) {
+            return 0;
+        }
+        //go back in the input and see where the first member of the operation starts
+
+        for (int i = index; i > 0; i--) {
+            if (i == 0) {
+                return 0;
+            }
+
+            char currChar = input.charAt(i);
+            if (currChar == '+' || currChar == '-' || currChar == '*' || currChar == '/') {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private int getSecondMemberIndex(String input, int index) {
+
+        //go forward in the input and see where the first member of the operation starts
+
+        for (int i = index + 1; i < input.length(); i++) {
+            char currChar = input.charAt(i);
+
+            if (currChar == '+' || currChar == '-' || currChar == '*' || currChar == '/' || i == input.length()) {
+                return i;
+            }
+        }
+
+        return input.length();
+    }
+
+
+    // TODO implement in JNI
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+
+    private int divide(int a, int b) {
+        return a / b;
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+
+    private int subtract(int a, int b) {
+        return a - b;
     }
 }
